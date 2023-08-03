1db55764a196be4276b171d7700063e2e74c86bd
SomnathKar000 <somnathkar201901@gmail.com>
2023-08-03 21:41:56 +0530

    new changes

--- src/handlers/generateUploadUrl.ts
+++ src/handlers/generateUploadUrl.ts
@@ -3,10 +3,10 @@ import { successResponse } from "../utils/response";
 import { errorResponse } from "../utils/errorHandler";
 
 export const handler = async (event: APIGatewayProxyEvent) => {
-  const { filePath } = JSON.parse(event.body!);
-  if (!filePath) {
+  const fileName = JSON.parse(event.body!).fileName;
+  if (!fileName) {
     return errorResponse("Invalid file path", 400);
   }
   const message = "Go Serverless v1.0! Your function executed successfully!";
-  return successResponse({ message, filePath });
+  return successResponse({ message, fileName });
 };
