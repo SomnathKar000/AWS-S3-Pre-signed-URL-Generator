aeb1d058ec0a08c22b6923e0c2975187a5dd9c64
SomnathKar000 <somnathkar201901@gmail.com>
2023-08-03 21:50:43 +0530

    upload url

--- src/handlers/generateUploadUrl.ts
+++ src/handlers/generateUploadUrl.ts
@@ -1,12 +1,14 @@
 import { APIGatewayProxyEvent } from "aws-lambda";
 import { successResponse } from "../utils/response";
 import { errorResponse } from "../utils/errorHandler";
+import { getUploadUrl } from "../services/s3Service";
 
 export const handler = async (event: APIGatewayProxyEvent) => {
   const fileName = JSON.parse(event.body!).fileName;
   if (!fileName) {
     return errorResponse("Invalid file path", 400);
   }
+  const url = await getUploadUrl(fileName);
   const message = "Go Serverless v1.0! Your function executed successfully!";
-  return successResponse({ message, fileName });
+  return successResponse({ message, fileName, url });
 };
--- src/services/s3Service.ts
+++ src/services/s3Service.ts
@@ -1,3 +1,12 @@
-// export const const getUploadUrl = () => {
+import { S3 } from "aws-sdk";
+const s3 = new S3();
+const bucketName = process.env.S3_BUCKET_NAME!;
 
-// }
+export const getUploadUrl = (key: string) => {
+  const url = s3.getSignedUrlPromise("putObject", {
+    Bucket: bucketName,
+    Key: key,
+    Expires: 60,
+  });
+  return url;
+};
