7152ee9bf5196a915563f7c89f11aa8e831f4483
SomnathKar000 <somnathkar201901@gmail.com>
2023-08-06 22:42:19 +0530

    new deploy

--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -6,12 +6,6 @@ export const errorResponse = (
 ): APIGatewayProxyResult => {
   return {
     statusCode: statusCode,
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "OPTIONS,POST,GET,PUT,DELETE",
-      "Access-Control-Allow-Headers": "Content-Type",
-    },
     body: JSON.stringify({ message, success: false }),
   };
 };
