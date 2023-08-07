26c198ed9fa8f12995469f4ad4257a31a6bfcbb3
SomnathKar000 <somnathkar201901@gmail.com>
2023-08-07 20:02:49 +0530

    new update

--- src/utils/response.ts
+++ src/utils/response.ts
@@ -4,6 +4,7 @@ export const successResponse = (body: any): APIGatewayProxyResult => {
   return {
     statusCode: 200,
     headers: {
+      "Content-Type": "application/json",
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Methods": "OPTIONS,POST,GET,PUT,DELETE",
       "Access-Control-Allow-Headers": "Content-Type",
