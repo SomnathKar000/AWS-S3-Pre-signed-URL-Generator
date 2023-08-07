02bebb38da6a64af3c6ce1e69d8561436023ef8b
SomnathKar000 <somnathkar201901@gmail.com>
2023-08-07 19:30:57 +0530

    new update

--- src/handlers/generateDownloadUrl.ts
+++ src/handlers/generateDownloadUrl.ts
@@ -15,6 +15,9 @@ export const handler = (event: APIGatewayProxyEvent) => {
   // });
   return {
     statusCode: 200,
+    headers: {
+      "content-type": "application/json",
+    },
     body: JSON.stringify({ message: "Successfully generated presigned URL" }),
   };
 };
