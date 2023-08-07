f8a9e4a754e738d481730c2f4bf7502b04185df9
SomnathKar000 <somnathkar201901@gmail.com>
2023-08-07 19:50:37 +0530

    new update

--- src/handlers/generateDownloadUrl.ts
+++ src/handlers/generateDownloadUrl.ts
@@ -4,17 +4,19 @@ import { errorResponse } from "../utils/errorHandler";
 import { successResponse } from "../utils/response";
 
 export const handler = (event: APIGatewayProxyEvent) => {
-  // const { fileName } = JSON.parse(event.body!);
-  // if (!fileName) {
-  //   return errorResponse("fileName is required", 400);
-  // }
-  // const url = getUrl(fileName, "getObject");
-  // return successResponse({
-  //   message: "Successfully generated presigned URL",
-  // url,
-  // });
+  const { fileName } = JSON.parse(event.body!);
+  if (!fileName) {
+    return errorResponse("fileName is required", 400);
+  }
+  const url = getUrl(fileName, "getObject");
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Successfully generated presigned URL" }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      message: "Successfully generated presigned URL",
+      url,
+    }),
   };
 };
