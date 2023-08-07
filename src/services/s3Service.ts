25f1a0ebc44d08f57f299c962b3c45bf9dfb8ad4
SomnathKar000 <somnathkar201901@gmail.com>
2023-08-07 18:47:30 +0530

    new update

--- src/services/s3Service.ts
+++ src/services/s3Service.ts
@@ -2,7 +2,7 @@ import { S3 } from "aws-sdk";
 const s3 = new S3();
 const bucketName = process.env.S3_BUCKET_NAME!;
 
-export const getUrl = (key: string, type: string) => {
+export const getUrl = (key: string, type: string): Promise<string> => {
   const url = s3.getSignedUrlPromise(type, {
     Bucket: bucketName,
     Key: key,
